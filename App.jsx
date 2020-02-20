5e44103c282d4165a2443a3889bf6822e668ae29
Barbara Mooney <60058920+barbara-mooney@users.noreply.github.com>
2020-02-19 23:48:16 -0800

    Update working version of to-do app

---
+++ App.jsx
@@ -0,0 +1,183 @@
+import React, { Component } from 'react';
+import ListPanel from './ListPanel';
+import Edit from './Edit';
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      key: '',
+      newTodo: '',
+      priority: '',
+      completed: false,
+      listofTodos: [],
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+    this.completeTodo = this.completeTodo.bind(this);
+    this.edit = this.edit.bind(this);
+    this.save = this.save.bind(this);
+    this.delete = this.delete.bind(this);
+  }
+
+handleInputChange(event) {
+  event.preventDefault()
+  this.setState({
+    [event.target.name]: event.target.value 
+    });
+  }
+
+addTodo (event) {
+  event.preventDefault()
+  const todo = this.state.newTodo;
+  const priorityLevel = this.state.priority;
+  const todoCompleted = this.state.completed;
+  const newTodoToadd = {
+    key: Date.now() + Math.random(),
+    newTodo: todo,
+    priority: priorityLevel,
+    completed: todoCompleted,
+    isBeingEdited: false,
+  }
+  this.setState ({
+    listofTodos: this.state.listofTodos.concat([newTodoToadd]),
+  });
+}
+
+completeTodo (event) {
+  this.setState({
+    [event.target.name]: event.target.value
+  })
+}
+
+edit (key) {
+  let tempList = this.state.listofTodos
+  tempList.forEach((item) => {
+    if (key==item.key) {
+      item.isBeingEdited = true;
+    }
+    this.setState ({
+      listofTodos: tempList
+    })
+})
+}
+
+delete (key) {
+  let tempList = this.state.listofTodos.filter(item => {
+    return item.key != key
+    })
+    this.setState ({
+      listofTodos: tempList
+    })
+}
+
+
+save (title, priority, key) {  
+  console.log(title)
+  let tempList = this.state.listofTodos
+  
+  tempList.forEach((item) => {
+    if (key==item.key) {
+      item.newTodo = title;
+      item.priority = priority;
+      item.isBeingEdited = false;
+    }
+    this.setState ({
+      listofTodos: tempList
+    })
+})
+}
+
+  render() {
+    let display;
+    let newArray;
+    if (this.state.listofTodos.length == 0) {
+      display = <div className="card-body">Welcome to Very Simple To-Do App! Get started now by adding a new to-do on the left.</div>
+    } else {
+      display = <div>
+        {this.state.listofTodos.map((item) => {
+          if (item.isBeingEdited == false) {
+            return (
+              <ListPanel
+                key={item.key}
+                id={item.key}
+                newTodo={item.newTodo}
+                priority={item.priority}
+                completed={item.completed}
+                edit={this.edit}
+                delete={this.delete}
+                />
+
+            )
+          } else {
+            return (
+              <Edit 
+                save={this.save}
+                key={item.key}
+                id={item.key}
+                title={item.newTodo}
+                />
+            )
+          }
+        })}
+      </div>
+    }
+    return (
+      <div>
+        <div className='container'>
+          <h1>Very Simple To-Do App</h1>
+          <h3>Track all of the things</h3>
+          <hr size="15px" />
+          <br/>
+          <div style={{display: 'flex'}}>
+          <div className="row">
+            <div className="col-4">
+              <div className="card">
+                <div className="card-header">
+                  Add New To-do
+                </div>
+                <div className="card-body">
+                  <div className="form-group">
+                    <label htmlFor="exampleFormControlTextarea1">I want to...</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"
+                    name="newTodo"
+                    onChange={this.handleInputChange}></textarea>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <label htmlFor="exampleFormControlSelect1">How much of a priority is this?</label>
+                  <select className="form-control" id="exampleFormControlSelect1" name="priority" onChange={this.handleInputChange}>
+                    <option>Select a priority</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                  </select>
+                </div>
+                <div className="card-footer text-muted">
+                  <button 
+                        type="button" 
+                        className="btn btn-success btn-block"   
+                        name="addTodo"
+                        onClick={this.addTodo}>Add</button>
+                </div>
+              </div>
+            </div>
+            <div className="col-8">
+              <div className="card">
+                <div className="card-header">
+                  View To-Dos
+                </div>
+                <div>
+                    {display}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Edit.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class Edit extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+        title: '',
+        priority: '',
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+}
+
+handleInputChange(event) {
+  // event.preventDefault()
+  this.setState({
+    [event.target.name]: event.target.value 
+    });
+  }
+
+render() {
+    return (
+    <div>
+      <div className="card-body">
+        <div className="form-group">
+          <label for="exampleFormControlTextarea1">Description</label>
+          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"
+          name="title" key={this.props.id} onChange={this.handleInputChange}value={this.state.title}></textarea>
+        </div>
+      </div>
+      <div className="card-body">
+        <label for="exampleFormControlSelect1">Priority</label>
+        <select className="form-control" id="exampleFormControlSelect1" name="priority" key={this.props.id} onChange={this.handleInputChange} value={this.state.priority}>
+          <option>Select a priority</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+        </select>
+      </div>
+      <div className="card-footer text-muted">
+        <button 
+              type="button" 
+              className="btn btn-success btn-block"   
+              name="addTodo"
+              onClick={ () => this.props.save(this.state.title, this.state.priority, this.props.id)}>Save</button>
+      </div>
+    </div>
+    );
+}
+}
+
+export default Edit;
